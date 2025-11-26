ae4fdd84dee4428587779baf2dd69553e961ee41
Atoosa-s <167201714+Atoosa-s@users.noreply.github.com>
2025-11-26 14:16:09 +0100

    Player

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    private float movement = 0f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = Input.GetAxis("Horizontal");
+
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position += new Vector3(movement, 0f, 0f) * Time.fixedDeltaTime;
+        
+
+    }
+}
