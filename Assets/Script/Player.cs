fe7503d70cf03317f56eedd14299cfaa8445d8d6
atosa-sh <215046310+atosa-sh@users.noreply.github.com>
2025-12-02 14:08:55 +0100

    Jump

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -2,9 +2,12 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-
+    public Rigidbody2D rb;
+    public float jumpHight = 5f;
     private float movement = 0f;
     public float moveSpeed = 5f;
+    private bool facingRight = true;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -17,12 +20,30 @@ public class Player : MonoBehaviour
     {
         movement = Input.GetAxis("Horizontal");
 
+        if (movement < 0f && facingRight)
+        {
+            transform.eulerAngles = new Vector3(0f, -180f, 0f);
+            facingRight = false;
+        }
+        else if (movement > 0f && facingRight == false)
+        {
+            transform.eulerAngles = new Vector3(0f, 0f, 0f);
+            facingRight = true;
+        }
+        if (Input.GetKey(KeyCode.Space)){
+            Jump();
+        }
     }
 
     private void FixedUpdate()
     {
         transform.position += new Vector3(movement, 0f, 0f) * Time.fixedDeltaTime * moveSpeed;
-        
 
+
+    }
+    
+    void Jump()
+    {
+        rb.AddForce(new Vector2(0f, jumpHight), ForceMode2D.Impulse);
     }
 }
