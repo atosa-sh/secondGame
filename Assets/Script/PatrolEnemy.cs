4211ed7b472f955023b2fd4dcc8a14c4c03621b8
atosa-sh <215046310+atosa-sh@users.noreply.github.com>
2026-03-09 14:38:58 +0100

    2:44

---
+++ Assets/Script/PatrolEnemy.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class PatrolEnemy : MonoBehaviour
+{
+    public float moveSpeed = 2f;
+    public Transform checkPoint;
+    public float distance = 1f;
+    public LayerMask layerMask;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.left * Time.deltaTime * moveSpeed);
+
+        RaycastHit2D hit = Physics2D.Raycast(checkPoint.position, Vector2.down, distance, layerMask);
+
+        if (hit == false)
+        {
+            Debug.Log("Flip");
+
+        }
+
+
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (checkPoint == null)
+        {
+            return;
+        }
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawRay(checkPoint.position, Vector2.down * distance);
+        
+    }
+
+
+
+
+}
