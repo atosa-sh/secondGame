30a5fca1ca1d3b6ce9d02ea74eb6e15d1dbe4ae2
atosa-sh <215046310+atosa-sh@users.noreply.github.com>
2026-03-29 15:11:43 +0200

    ButtonAndMenuDone

--- Assets/Script/SceneManagement.cs
+++ Assets/Script/SceneManagement.cs
@@ -3,8 +3,18 @@ using UnityEngine.SceneManagement;
 public class SceneManagement : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+   
+   
+   public void PlayGame()
+    {
+        SceneManager.LoadScene("m");
+    }
     public void LoadLevel()
     {
         SceneManager.LoadScene("Level2");
     }
+    public void QuitGame()
+    {
+        Application.Quit();
+        }
 }
